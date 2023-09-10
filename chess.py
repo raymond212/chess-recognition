532c97d136e8cc001221e4df4f15ee3ec99abf51
Raymond <ruiwenhw@gmail.com>
2023-09-09 21:49:16 -0700

    Implement FEN prediction

--- chess.py
+++ chess.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-from main import predict, resize, size
+from main import predict, resize
 
 label_short = {
     'wk': 'K',
@@ -20,7 +20,7 @@ label_short = {
 
 
 def main():
-    img = cv.imread('images/boards_testing/board5.png', )
+    img = cv.imread('images/boards_testing/board8.png', )
 
     canny = cv.Canny(img, 125, 175)
     # cv.imshow('canny', canny)
@@ -33,40 +33,65 @@ def main():
 
     s = ''
 
+    board_cnt = None
+
     for cnt in contours:
-        x, y, w, h = cv.boundingRect(cnt)
-        print((x, y, w, h))
-        if abs(w / h - 1) <= 0.01:
-            cv.drawContours(img, [cnt], -1, (0, 255, 0), 1)
-            gw, gh = w / 8, h / 8
-
-            for row in range(8):
-                for col in range(8):
-                    x1, x2 = x + round(gw * col), x + round(gw * (col + 1))
-                    y1, y2 = y + round(gh * row), y + round(gh * (row + 1))
-                    # cv.circle(img, (x1, y1), 10, (0, 0, 255), -1)
-                    # cv.circle(img, (x1, y2), 10, (0, 0, 255), -1)
-                    # cv.circle(img, (x2, y1), 10, (0, 0, 255), -1)
-                    # cv.circle(img, (x2, y2), 10, (0, 0, 255), -1)
-
-                    cell = img[y1:y2, x1:x2]
-                    cell = cv.resize(cell, dsize=(size, size), interpolation=cv.INTER_CUBIC)
-                    # cv.imshow('cell', cell)
-                    # cv.waitKey(0)
-                    predicted_piece = label_short[predict(cell)]
-                    # print(predicted_piece)
-                    s += '|' + predicted_piece
-                s += '|\n'
-
-            print(gw, gh)
-
-            print(s)
-
+        _, _, w, h = cv.boundingRect(cnt)
+        if h > 0 and abs(w / h - 1) <= 0.05:
+            board_cnt = cnt
             break
 
-    cv.imshow('board', img)
+    if board_cnt is None:
+        print("No chessboard detected.")
+    else:
+        x, y, w, h = cv.boundingRect(board_cnt)
+        cv.drawContours(img, [board_cnt], -1, (0, 255, 0), 1)
 
-    cv.waitKey(0)
+        gw, gh = w / 8, h / 8
+
+        board = [[""] * 8 for _ in range(8)]
+
+        for row in range(8):
+            for col in range(8):
+                x1, x2 = x + round(gw * col), x + round(gw * (col + 1))
+                y1, y2 = y + round(gh * row), y + round(gh * (row + 1))
+
+                cv.circle(img, (x1, y1), 10, (0, 0, 255), -1)
+                cv.circle(img, (x1, y2), 10, (0, 0, 255), -1)
+                cv.circle(img, (x2, y1), 10, (0, 0, 255), -1)
+                cv.circle(img, (x2, y2), 10, (0, 0, 255), -1)
+
+                cell = resize(img[y1:y2, x1:x2])
+                # cv.imshow('cell', cell)
+                # cv.waitKey(0)
+                predicted_piece = label_short[predict(cell)]
+                board[row][col] = predicted_piece
+
+        print('\n'.join([f'|{"|".join(rank)}|' for rank in board]))
+        print(board_to_fen(board) + " w - - 0 1")
+
+        cv.imshow('board', img)
+        cv.waitKey(0)
+
+
+def board_to_fen(board):
+    res = []
+    for rank in board:
+        rank_str = ""
+        empty_counter = 0
+        for square in rank:
+            if square == "_":
+                empty_counter += 1
+                continue
+            if empty_counter > 0:
+                rank_str += str(empty_counter)
+                empty_counter = 0
+            rank_str += square
+        if empty_counter > 0:
+            rank_str += str(empty_counter)
+        res.append(rank_str)
+    return '/'.join(res)
+
 
 
 if __name__ == '__main__':
